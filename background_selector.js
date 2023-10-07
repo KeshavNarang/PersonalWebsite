501c4316d2e0bd14faf2cf7ac29211fd1db5ad25
Keshav Narang <mrkeshavnarang@gmail.com>
2023-10-06 21:14:35 -0700

    Added new feature

--- background_selector.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var header = document.getElementById("intro-header");
-    var images = ["sunset_ocean_one.jpg", "sunset_ocean_two.jpg"];
-    var randomImage = images[Math.floor(Math.random() * images.length)];
-    header.style.backgroundImage = "url(" + randomImage + ")";
-});
